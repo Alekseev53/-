28f69f2722df6cb11151bd4e680ed2a7117f8550
silach53 <48329361+silach53@users.noreply.github.com>
2023-08-14 18:37:50 +0300

    optimizing code

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 a = [9,5,8,1,2]
 
-for _ in range(len(a)-1):
-    for i in range(1,len(a)):
+for j in range(len(a)-1):
+    for i in range(1,len(a)-j):
         if a[i]<a[i-1]:
             a[i],a[i-1] = a[i-1],a[i]
 print(a)
